3d559b34f052cb1673826f452ede6d5e428dce62
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-05-30 05:22:48 +0900

    moved some methods

--- lib/enju_inventory.rb
+++ lib/enju_inventory.rb
@@ -1,4 +1,21 @@
 require "enju_inventory/engine"
 
 module EnjuInventory
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def enju_inventory
+      include EnjuInventory::InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    def get_inventory_file
+      @inventory_file = InventoryFile.find(params[:inventory_file_id]) if params[:inventory_file_id]
+    end
+  end
 end
+
+ActionController::Base.send(:include, EnjuInventory)
