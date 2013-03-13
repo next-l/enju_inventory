7eb958c5f5bcc87864726382d45ef433e60b1d11
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-03-13 19:32:57 +0900

    fixed defining method

--- lib/enju_inventory/item.rb
+++ lib/enju_inventory/item.rb
@@ -12,21 +12,19 @@ module EnjuInventory
         searchable do
           integer :inventory_file_ids, :multiple => true
         end
-      end
-    end
 
-    module InstanceMethods
-      def self.inventory_items(inventory_file, mode = 'not_on_shelf')
-        item_ids = Item.pluck(:id)
-        inventory_item_ids = inventory_file.items.pluck('items.id')
-        case mode
-        when 'not_on_shelf'
-          Item.where(:id => (item_ids - inventory_item_ids))
-        when 'not_in_catalog'
-          Item.where(:id => (inventory_item_ids - item_ids))
+        def inventory_items(inventory_file, mode = 'not_on_shelf')
+          item_ids = Item.pluck(:id)
+          inventory_item_ids = inventory_file.items.pluck('items.id')
+          case mode
+          when 'not_on_shelf'
+            Item.where(:id => (item_ids - inventory_item_ids))
+          when 'not_in_catalog'
+            Item.where(:id => (inventory_item_ids - item_ids))
+          end
+        rescue
+          nil
         end
-      rescue
-        nil
       end
     end
   end
