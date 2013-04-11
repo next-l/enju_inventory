4d6c70c24028c1306bec69ce20366cb4ed97b987
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-12 02:26:12 +0900

    removed generator

--- lib/generators/enju_inventory/setup/setup_generator.rb
+++
@@ -1,8 +0,0 @@
-class EnjuInventory::SetupGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-
-  def setup
-    inject_into_class 'app/models/item.rb', Item,
-      "  enju_inventory_item_model\n"
-  end
-end
