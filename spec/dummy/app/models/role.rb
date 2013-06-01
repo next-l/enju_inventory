310d046e6d70740d241dace1e1347e08e41535c4
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-06-01 19:38:02 +0900

    removed dummy files

--- spec/dummy/app/models/role.rb
+++
@@ -1,5 +0,0 @@
-class Role < ActiveRecord::Base
-  def self.default_role
-    Role.where(:name => 'Guest').first
-  end
-end
--- spec/dummy/app/models/user_has_role.rb
+++
@@ -1,4 +0,0 @@
-class UserHasRole < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :role
-end
