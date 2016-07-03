703f358dabb412fe69c3b553244a9bc6a2e6a54e
Kosuke Tanabe <nabeta@fastmail.fm>
2016-07-04 01:58:53 +0900

    move some modules

--- lib/enju_inventory.rb
+++ lib/enju_inventory.rb
@@ -1,23 +1,4 @@
 require "enju_inventory/engine"
-require "enju_inventory/item"
 
 module EnjuInventory
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
-
-  module ClassMethods
-    def enju_inventory
-      include EnjuInventory::InstanceMethods
-    end
-  end
-
-  module InstanceMethods
-    def get_inventory_file
-      @inventory_file = InventoryFile.find(params[:inventory_file_id]) if params[:inventory_file_id]
-    end
-  end
 end
-
-ActiveRecord::Base.send :include, EnjuInventory::InventoryItem
-ActionController::Base.send(:include, EnjuInventory)
--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
 
   include EnjuLeaf::Controller
   include EnjuLibrary::Controller
+  include EnjuInventory::Controller
   after_action :verify_authorized
+  before_action :set_paper_trail_whodunnit
 
   include Pundit
 end
