a1b8cc1b6dc34000a42359f3c96a1b072cdf7e58
Kosuke Tanabe <nabeta@fastmail.fm>
2017-01-10 00:06:33 +0900

    fix merge

--- app/models/inventory_file.rb
+++ app/models/inventory_file.rb
@@ -1,18 +1,9 @@
 class InventoryFile < ActiveRecord::Base
-<<<<<<< HEAD
-  has_many :inventories, dependent: :destroy
-  has_many :items, through: :inventories
-  belongs_to :user
-  validates_presence_of :user
-
-  include AttachmentUploader[:attachment]
-=======
   include AttachmentUploader[:attachment]
   has_many :inventories, dependent: :destroy
   has_many :items, through: :inventories
   belongs_to :user
   validates :user, presence: true
->>>>>>> 1570fd2e76233bbf84d15be1c8710cd9a1e569fe
   validates :attachment, presence: true, on: :create
 
   paginates_per 10
