bb275245a07adffb601d930a1eee75d27f508809
Kosuke Tanabe <nabeta@fastmail.fm>
2018-09-22 20:43:10 +0900

    update dummy files

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  include EnjuBiblio::Controller
   include EnjuLibrary::Controller
   include EnjuInventory::Controller
   after_action :verify_authorized
