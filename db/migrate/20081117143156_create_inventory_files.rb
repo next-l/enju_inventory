2a65a9b2159edcf4dfae1bb62e76e24e98ffdc4e
Kosuke Tanabe <nabeta@fastmail.fm>
2019-02-24 03:32:17 +0900

    use uuid as primary key in InventoryFile

--- db/migrate/20081117143156_create_inventory_files.rb
+++ db/migrate/20081117143156_create_inventory_files.rb
@@ -1,17 +1,10 @@
 class CreateInventoryFiles < ActiveRecord::Migration[5.2]
-  def self.up
-    create_table :inventory_files do |t|
-      t.string :filename
-      t.string :content_type
-      t.integer :size
-      t.references :user, index: true
+  def change
+    create_table :inventory_files, id: :uuid do |t|
+      t.references :user, foreign_key: true, null: false
       t.text :note
 
       t.timestamps
     end
-  end
-
-  def self.down
-    drop_table :inventory_files
   end
 end
