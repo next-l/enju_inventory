aef642dfb5c5a58b15b70c905b09289895861d13
Kosuke Tanabe <nabeta@fastmail.fm>
2020-01-05 15:32:50 +0900

    require enju_circulation

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -4,12 +4,13 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 require "enju_inventory"
+require "enju_circulation"
 require "enju_leaf"
 
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
