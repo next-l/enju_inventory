bf6dfa6532c3a345810ae11f822e1bbe2d3ac829
Kosuke Tanabe <nabeta@fastmail.fm>
2021-03-24 01:19:41 +0900

    move enju_inventory:item:import task to the instance method of InventoryFile

--- app/models/inventory_file.rb
+++ app/models/inventory_file.rb
@@ -10,15 +10,75 @@ class InventoryFile < ActiveRecord::Base
   paginates_per 10
 
   def import
-    inventory.download.split.each do |row|
-      identifier = row.to_s.strip
-      item = Item.find_by(item_identifier: identifier)
-      Inventory.create(
-        inventory_file: self,
-        item: item,
-        current_shelf_name: shelf.name,
-        item_identifier: identifier
-      )
+    CSV.parse(inventory.download, headers: true, col_sep: "\t") do |row|
+      # 電子書籍を除外
+      next if row['shelf'] == 'web'
+      item_identifier row['item_identifier'].to_s.strip
+      next if item_identifier.blank?
+
+      Item.transaction do
+        item = Item.find_by(item_identifier: item_identifier)
+        Inventory.create(
+          inventory_file: self,
+          item: item,
+          current_shelf_name: shelf.name,
+          item_identifier: item_identifier
+        )
+
+        next unless item
+
+        case row['circulation_status']
+        when 'On Loan'
+          # 書架にないと報告されたが、確認したら貸出中の場合、不明日を削除
+          if item.missing_since.present?
+            item.update!(missing_since: nil)
+            Rails.logger.info "Inventory: #{row['item_identifier']}: wrong report (rent)"
+          end
+        when 'Available On Shelf'
+          # 書架にないと報告されたが、確認したら書架にあった本を「利用可能」に変更
+          if row['current_shelf'] == '-' or row['current_shelf'].blank?
+            if item.missing_since.present? && !item.rent?
+              item.update!(missing_since: nil, circulation_status: CirculationStatus.find_by(name: row['circulation_status']))
+              Rails.logger.info "Inventory: #{row['item_identifier']}: wrong report (on shelf)"
+            end
+          end
+
+          # 発見された不明本の状態を変更
+          unless row['current_shelf'] == '-' or row['current_shelf'].blank?
+            if row['found_at'].present?
+              if item.rent?
+                item.update!(missing_since: nil)
+              else
+                item.update!(
+                  missing_since: nil,
+                  circulation_status: CirculationStatus.find_by(name: row['circulation_status'])
+                )
+              end
+              Rails.logger.info "Inventory: #{row['item_identifier']}: changed status to found"
+            end
+          end
+        when 'Missing'
+          item.circulation_status = CirculationStatus.find_by(name: 'Missing') unless item.circulation_status == 'Missing'
+          if row['missing_since'].blank?
+            item.update!(missing_since: Date.today)
+            Rails.logger.info "Inventory: #{row['item_identifier']}: changed status to missing"
+          else
+            item.update!(missing_since: Date.parse(row['missing_since']))
+            Rails.logger.info "Inventory: #{row['item_identifier']}: changed status to missing"
+          end
+        end
+
+        # 配架場所の変更を反映
+        unless row['current_shelf'] == row['shelf']
+          unless row['current_shelf'] == '-' or row['current_shelf'].blank?
+            shelf = Shelf.find_by(name: row['current_shelf'])
+            if item && shelf
+              item.update!(shelf: Shelf.find_by(name: row['current_shelf']))
+              Rails.logger.info "Inventory: #{item.item_identifier}: shelf updated to #{shelf.name}"
+            end
+          end
+        end
+      end
     end
   end
 
